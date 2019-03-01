1a1ab848115a01661fd141f6ebb7552d7978b188
pvvarun <varun.mishra@successive.tech>
2019-03-01 18:17:31 +0530

    feature: Material UI, Styling(#40385)

--- training/src/configs/constants.js
+++ training/src/configs/constants.js
@@ -1,3 +1,5 @@
+import * as yup from 'yup';
+
 export const PUBLIC_IMAGE_FOLDER = '/images';
 export const DEFAULT_BANNER_IMAGE = '/banners/default.png';
 export const sport = [
@@ -9,3 +11,15 @@ export const object1 = {
   cricket: [{ label: 'Batsman', value: 'batsman' }, { label: 'Bowler', value: 'bowler' }, { label: 'WicketKeeper', value: 'wicketKeeper' }, { label: 'AllRounder', value: 'allrounder' }],
   football: [{ label: 'Striker', value: 'striker' }, { label: 'Defender', value: 'defender' }, { label: 'GoalKeeper', value: 'goalkeeper' }],
 };
+
+export const AddDialogSchema = yup.object({
+  Name: yup.string().min(3).required().label('Name'),
+  EmailAddress: yup.string()
+    .matches(/^[A-Za-z0-9._%+-]+@successive.tech$/, 'EmailAddress must be a valid email.')
+    .required('Email Address is required'),
+  Password: yup.string()
+    .matches(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,15}$/, 'Password must be minimum of 8 characters and contains at least one Uppercase letter, lowercase letter and one digit.')
+    .required('Password is required.'),
+  ConfirmPassword: yup.string()
+    .oneOf([yup.ref('Password'), null], 'Passwords must match'),
+});
---
+++ training/src/pages/Trainee/Trainee.jsx
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import Button from '@material-ui/core/Button';
+// import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import { AddDialog } from './components/AddDialog';
+
+const styles = theme => ({
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing.unit,
+    marginRight: theme.spacing.unit,
+  },
+  dense: {
+    marginTop: 16,
+  },
+  menu: {
+    width: 200,
+  },
+});
+
+
+class OutlinedTextFields extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      open: false,
+    };
+  }
+
+  handleClickOpen = () => {
+    this.setState(preState => ({ open: !preState.open }));
+  };
+
+  render() {
+    const { open } = this.state;
+    console.log(open, 'inside traine  open state');
+    return (
+      <>
+        <Button variant="outlined" color="primary" onClick={this.handleClickOpen}>
+          ADD TRAINEE
+        </Button>
+        <AddDialog open={open} show={this.handleClickOpen} />
+
+
+      </>
+    );
+  }
+}
+
+export default withStyles(styles)(OutlinedTextFields);
